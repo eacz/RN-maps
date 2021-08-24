5d7467972c91bb7bb74f5aa3d03db26b824c5b71
Esteban Canteros <estebanacanteros@gmail.com>
2021-08-24 10:15:16 -0300

    asking for permissions

--- src/screens/PermissionScreen.tsx
+++ src/screens/PermissionScreen.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Button, Platform } from 'react-native'
+import { PERMISSIONS, PermissionStatus, request } from 'react-native-permissions'
 
 const PermissionScreen = () => {
+  const checkLocationPermissions = async () => {
+      let permissionStatus: PermissionStatus;
+      if(Platform.OS === 'ios'){
+         permissionStatus = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE)
+        } else {
+          permissionStatus = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION)
+      }
+      console.log(permissionStatus);
+      
+  }
   return (
-    <View>
+    <View style={styles.container} >
       <Text>PermissionScreen</Text>
+      <Button title="Permissions" onPress={checkLocationPermissions} />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
+});
 
 export default PermissionScreen
