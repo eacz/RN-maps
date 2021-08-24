a51155bbe6666cf40f94cd84cf830d4623f380c5
Esteban Canteros <estebanacanteros@gmail.com>
2021-08-24 20:55:10 -0300

    showing screens based on permissions

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -1,21 +1,32 @@
-import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack';
-import PermissionScreen from '../screens/PermissionScreen';
-import MapScreen from '../screens/MapScreen';
+import React, { useContext } from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import PermissionScreen from '../screens/PermissionScreen'
+import MapScreen from '../screens/MapScreen'
+import { PermissionsContext } from '../context/PermissionsContext'
+import LoadingScreen from '../screens/LoadingScreen'
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 const Navigator = () => {
+  const {permissions} = useContext(PermissionsContext)
+
+  if(permissions.locationStatus === 'unavailable'){
+    return <LoadingScreen />
+  }
+  
   return (
     <Stack.Navigator
       screenOptions={{
-        headerShown: false
+        headerShown: false,
       }}
     >
-      <Stack.Screen name="PermissionScreen" component={PermissionScreen} />
-      <Stack.Screen name="MapScreen" component={MapScreen} />
+      {
+        permissions.locationStatus === 'granted' 
+          ? <Stack.Screen name='MapScreen' component={MapScreen} />
+          : <Stack.Screen name='PermissionScreen' component={PermissionScreen} />
+      }
     </Stack.Navigator>
-  );
+  )
 }
 
 export default Navigator
--- src/screens/LoadingScreen.tsx
+++ src/screens/LoadingScreen.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native'
 
 const LoadingScreen = () => {
   return (
-    <View>
-      <Text>LoadingScreen</Text>
+    <View style={styles.container}>
+      <ActivityIndicator size={50} color="grey" />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    }
+});
 
 export default LoadingScreen
