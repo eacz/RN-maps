7fc9822e20e75ed70a9e7246c885e3f3de607a6e
Esteban Canteros <estebanacanteros@gmail.com>
2021-08-24 23:07:44 -0300

    initial config for maps in android

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import MapView from 'react-native-maps';
 import { View, Text } from 'react-native'
 
 const MapScreen = () => {
   return (
-    <View>
+    <View style={{flex: 1}}>
       <Text>MapScreen</Text>
+      <MapView
+        style={{flex: 1}}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   )
 }
 
 export default MapScreen
+
+//AIzaSyB6jrBKeiVP81gQoVLeBqtoVtV_Ulksjjo
