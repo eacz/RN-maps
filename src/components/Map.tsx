c4dec45fe0673d438dd3c7fc4e8c0b8c2060dda7
Esteban Canteros <estebanacanteros@gmail.com>
2021-08-25 10:21:43 -0300

    getting user geolocation

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import MapView, { Marker } from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
 interface Props {
   markers?: Marker[]
 }
 
 const Map = ({markers = []}: Props) => {
+  Geolocation.getCurrentPosition(
+    info => console.log(info),
+    err => console.log(err),
+    {
+      enableHighAccuracy: true
+    }
+    
+  );
   return (
     <>
        <MapView
@@ -20,12 +29,11 @@ const Map = ({markers = []}: Props) => {
       >
         <Marker
           image={require('../assets/custom-marker.png')}
+          title="yyo" description="yyo en resi"
           coordinate={{
             latitude: -27.4257175,
             longitude: -59.0243784
           }}
-          title="yyo"
-          description="yyo en resi"
         />
       </MapView>
     </>
